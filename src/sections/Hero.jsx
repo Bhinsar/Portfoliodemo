a78577da473ce953924f1dfe4a06d19b7123de3a
Bhinsar Jagdish <bhinsarjagdish2003@gmail.com>
2025-05-06 20:45:06 +0530

    refactor: enhance Hero component layout and structure
    Updated the Hero component to include a more complex layout with a background image and structured header. This change improves the visual presentation and prepares the component for further content integration.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,17 @@
 function Hero() {
-    return (
-        <h1>Hero</h1>
-    )
+  return (
+    <section id="hero" className="relative overflow-hiddne">
+      <div  className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt='background'/>
+      </div>
+      <div>
+        {/* left : hero section */}
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+            <div className="flex flex-col gap-7"></div>
+        </header>
+        {/* right : hero section */}
+      </div>
+    </section>
+  );
 }
-export default Hero
+export default Hero;
