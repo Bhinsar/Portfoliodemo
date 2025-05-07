20b349e6e8424b7ea4ba79bdb9f46f32ab598267
Bhinsar Jagdish <bhinsarjagdish2003@gmail.com>
2025-05-07 20:52:20 +0530

    feat: enhance Hero section with animated text and layout adjustments
    Updated the Hero component to include animated text transitions and a refined layout. Added utility classes for centering and improved styling for the hero text, enhancing the overall visual appeal and interactivity of the section.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,13 +1,32 @@
+import { words } from "../Constants";
 function Hero() {
   return (
     <section id="hero" className="relative overflow-hiddne">
-      <div  className="absolute top-0 left-0 z-10">
-        <img src="/images/bg.png" alt='background'/>
+      <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="background" />
       </div>
-      <div>
+      <div className="hero-layout">
         {/* left : hero section */}
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
-            <div className="flex flex-col gap-7"></div>
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Shapping
+                <sapn className="slide">
+                  <span className="wrapper">
+                    {words.map((word, index) => (
+                      <span key={index} className="flex items-center mp:gap-3 gap-1 pb-2">
+                        <img src={word.imgPath} alt={word.text} className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"/>
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </sapn>
+              </h1>
+              <h1>Into Real Project</h1>
+              <h1>That Deliver Results</h1>
+            </div>
+          </div>
         </header>
         {/* right : hero section */}
       </div>
