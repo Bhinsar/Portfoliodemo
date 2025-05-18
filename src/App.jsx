cd49d6a8b4015cb31183c0190e9cd6aa2722c012
Bhinsar Jagdish <bhinsarjagdish2003@gmail.com>
2025-05-18 23:06:06 +0530

    feat: add Navbar component to App layout
    Integrated the Navbar component into the App structure, enhancing navigation and overall user experience. This addition complements the existing Hero and ShowcaseSection components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./sections/Hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
+import Navbar from "./Components/Navbar/navbar"
 function App() {
 
   return (
     <>
+      <Navbar/>
       <Hero/>
       <ShowcaseSection/>
     </>
